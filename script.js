f23c02a8f6f0e457f36217f39782611e8dae9aef
SHANA SHABNAM NASIM <142229568+shananasim16@users.noreply.github.com>
2023-11-26 12:13:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function focusNext(currentInput) {
+    if (currentInput.value.length === 1) {
+        const nextInput = currentInput.nextElementSibling;
+        if (nextInput) {
+            nextInput.focus();
+        }
+    }
+}
 
+function handleBackspace(currentInput, event) {
+    if (currentInput.value === '' && event.key === 'Backspace') {
+        const previousInput = currentInput.previousElementSibling;
+        if (previousInput) {
+            previousInput.focus();
+        }
+    }
+}
